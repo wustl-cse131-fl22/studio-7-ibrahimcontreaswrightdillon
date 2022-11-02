28d86ae031f1c69338b1230fc383e5159393c6dd
jasmi <jasmi@JasmineSurfaceStudio.dhcp.wustl.edu>
2022-11-02 15:47:04 -0500

    hi

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -4,16 +4,21 @@ public class Die {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Die test = new Die (6);
-System.out.print(test.returned());
+		Die test = new Die (6);
+		System.out.print(test.returned());
 	}
-private int n;
-public Die(int n) {
-	this.n = n;
-}
-public int returned() {
-	int range = this.n-1+1;
-	return(int) (Math.random()* range)+1;
-}
+	
+	
+	private int n;
+	public Die(int n) {
+		this.n = n;
+	}
+	
+	
+	
+	public int returned() {
+		int range = this.n-1+1;
+		return(int) (Math.random()* range)+1;
+	}
 
 }
--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -6,5 +6,55 @@ public class Fraction {
 		// TODO Auto-generated method stub
 
 	}
+	private int numerator;
+	private int denominator;
+
+
+	public Fraction(int numerator, int denominator) {
+		super();
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+
+
+
+	public Fraction sum(Fraction fraction2) {
+		Fraction answer = new Fraction((this.numerator*fraction2.denominator + fraction2.numerator*this.denominator) , (this.denominator*fraction2.denominator));
+		return answer;
+	}
+
+
+
+	public Fraction product(Fraction fraction2 ) {
+		Fraction answer = new Fraction(this.numerator*fraction2.numerator, this.denominator*fraction2.denominator);
+		return answer;
+	}
+
+
+
+	public String reciprocal() {
+		return denominator+"/"+ numerator;
+	}
+
+	
+	public static int getGcd(int p, int q) {
+		if (p % q ==0) {
+			return q;
+		}
+		else {
+			return getGcd (q,p % q);
+			}					
+			
+	}
+	
+	
+	
+	
+	public String simplification() {
+		int Gcd = getGcd(numerator, denominator);
+			return numerator/this.()+ "/"+ denominator/getGcd();
+		}
+	}
+
 
 }
